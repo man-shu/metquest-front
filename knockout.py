508e8aebbef1a88bfea29a183752f94f91fdcc62
man-shu <35485429+man-shu@users.noreply.github.com>
2018-07-24 13:47:53 +0530

    file paths generalised in knockout.py

--- knockout.py
+++ knockout.py
@@ -17,14 +17,17 @@ from collections import Counter
 import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-
+from datetime import datetime
 
 ALLOWED_EXTENSIONS = set(['xml'])
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-app.config['UPLOAD_PATH'] = '/home/himanshu/Desktop/uploads/'
+app.config['UPLOAD_PATH'] = os.path.join(os.getcwd(), datetime.now().strftime('KOFILES_%d-%m-%Y_%I:%M:%S'))
+if not os.path.exists(os.path.join(app.config['UPLOAD_PATH'])):
+    os.makedirs(os.path.join(app.config['UPLOAD_PATH']))
+    print('file made')
 
 class Knock(Form):
     length = TextField('Cut-off:', validators=[validators.required()])
@@ -109,248 +112,12 @@ def uploader():
       print(uploadForm.errors)
       return render_template('knockUpload.html')
 
-
-#@app.route('/full1', methods = ['GET', 'POST'])
-#def full1():
-#    global pathsHTML
-#    if request.method == 'POST':
-#        ind = request.form["full"]
-#        ind = int(ind)
-#        pathway1 = pathsHTML[ind]
-#        return render_template('path1.html', pathway1 = pathway1)
-
-#@app.route('/full2', methods = ['GET', 'POST'])
-#def full2():
-#    global pathsUnion
-#    if request.method == 'POST':
-#        ind = request.form["unFull"]
-#        ind = int(ind)
-#        pathway2 = pathsUnion[ind]
-#        return render_template('path2.html', pathway2 = pathway2)
-
-
-#def create_pathway(namemap, G, r):
-#    form = MetQuest(request.form)
-
-#    cofacs = ["ppi_m","pi_m","adp_m","atp_m","nh4_c","h_p","h_c","atp_c","adp_c","nadph_c","nadp_c","nad_c","nadh_c","h2o_m","co2_c","h_m","ade_c","amp_c","h2o_c","pi_c","pi_p","ade_p"]
-#    global modids
-#    rxn = []
-#    for a in range(len(r)):
-#        rxn.append(namemap[r[a]])
-#    mets = []
-#    d = {"reactions":[{"id":"R1","name":"","metabolites":{},"lower_bound":0,"upper_bound":1000,"gene_reaction_rule":"","notes":{"map_info":{"reversibility":False,"hidden":False,"cofactors":{}}}}],"metabolites":[{"id":"A","name":"","compartment":"","notes":{"map_info":{"display_name":"A"}}}],"genes":[],"id":"simple_model","compartments":{},"notes":{"map_info":{}}}
-
-#    for a in range(len(r)-1):
-#        d["reactions"].append({"id":"R1","name":"","metabolites":{},"lower_bound":0,"upper_bound":1000,"gene_reaction_rule":"","notes":{"map_info":{"flux":9,"reversibility":False,"hidden":False,"cofactors":{}}}})
-
-#    for a in range(len(rxn)):
-#        d["reactions"][a]["id"] = r[a].replace(" ", "")
-#        d["reactions"][a]["notes"]["map_info"]["display_name"] = rxn[a]
-#        preds = list(G.predecessors(r[a]))
-#        succs = list(G.successors(r[a]))
-#        for b in range(len(preds)):
-#            mets.append(preds[b])
-#            d["reactions"][a]["metabolites"][preds[b].replace(" ", "")] =-1
-#            if len(preds[b].split(" ",1)) == 1:
-#                if preds[b].split(" ",1)[0] in cofacs:
-#                    d["reactions"][a]["notes"]["map_info"]["cofactors"][preds[b].replace(" ", "")] ={}
-#            else :
-#                if preds[b].split(" ",1)[1] in cofacs:
-#                    d["reactions"][a]["notes"]["map_info"]["cofactors"][preds[b].replace(" ", "")] ={}
-#        for c in range(len(succs)):
-#            mets.append(succs[c])
-#            d["reactions"][a]["metabolites"][succs[c].replace(" ", "")] =1
-#            if len(succs[c].split(" ",1)) == 1:
-#                if succs[c].split(" ",1)[0] in cofacs:
-#                    d["reactions"][a]["notes"]["map_info"]["cofactors"][succs[c].replace(" ", "")] ={}
-#            else :
-#                if succs[c].split(" ",1)[1] in cofacs:
-#                    d["reactions"][a]["notes"]["map_info"]["cofactors"][succs[c].replace(" ", "")] ={}
-
-#    mets = list(set(mets))
-
-#    for a in range(len(mets)-1):
-#        d["metabolites"].append({"id":"A","name":"","compartment":"","notes":{"map_info":{"display_name":"A"}}})
-
-#    for a in range(len(mets)):
-#        d["metabolites"][a]["id"] = mets[a].replace(" ", "")
-#        if len(modids) == 1:
-#            d["metabolites"][a]["notes"]["map_info"]["display_name"] = mets[a].split(" ",1)[1]
-#        else:
-#            d["metabolites"][a]["notes"]["map_info"]["display_name"] = mets[a]
-#        if len(mets[a].split(" ",1)) == 1:
-#            if mets[a].split(" ",1)[0] in cofacs:
-#                d["metabolites"][a]["notes"]["map_info"]["display_name"] = mets[a].split(" ",1)[1]
-#                d["metabolites"][a]["notes"]["map_info"]["hidden"] =True
-#        else:
-#            if mets[a].split(" ",1)[1] in cofacs:
-#                d["metabolites"][a]["notes"]["map_info"]["display_name"] = mets[a].split(" ",1)[1]
-#                d["metabolites"][a]["notes"]["map_info"]["hidden"] =True
-#    return d
-
-#def print_summary(scope, tar_met, pathways, cut_len, cyclic, namemap, src_met, seed_met, G, all_tar2src, diff_tar2src):
-##    global pathway1
-##    pathway1 = create_pathway(seed_met, cut_len, src_met, tar_met, 0, most_diff, namemap, G)
-##    global pathway2
-##    pathway2 = create_pathway(seed_met, cut_len, src_met, tar_met, 1, most_diff, namemap, G)
-#    global pathsHTML
-#    global pathsUnion
-#    pathsHTML = []
-#    labels = []
-#    pathsUnion = []
-#    labelsUnion = []
-#    labelsSummary = []
-#    len_scopes = []
-#    cut_branched_pathss = []
-#    all_cnts = []
-#    cut_cnts = []
-#    cut_cyclics = []
-#    minstepss = []
-#    for i in range(len(tar_met)):
-#        for j in range(len(src_met)):
-#            labelSummary = "%s %s to %s summary"%("_".join(modids), src_met[j], tar_met[i])
-#            labelsSummary.append(labelSummary)
-#            if len(diff_tar2src[tar_met[i]][src_met[j]]) == 0:
-#                if len(all_tar2src[tar_met[i]][src_met[j]]) == 0:
-#                    print('No pathway found for %s to %s'%(src_met[j],tar_met[i]))
-#                    pathsHTML.append(None)
-#                    label  = "%s %s to %s no pathway"%("_".join(modids), src_met[j], tar_met[i])
-#                    labels.append(label)
-
-#                    pathsUnion.append(None)
-#                    labelsUnion.append(label)
-
-#                    len_scopes.append(len(scope))
-#                    cut_branched_paths = len(all_tar2src[tar_met[i]][src_met[j]])
-#                    cut_branched_pathss.append(cut_branched_paths)
-#                    all_cnts.append(None)
-#                    cut_cnts.append(None)
-#                    cut_cyclics.append(None)
-#                    minstepss.append(None)
-#                else:
-#                    print('just one pathway found for %s to %s'%(src_met[j],tar_met[i]))
-#                    k = 0
-#                    r = all_tar2src[tar_met[i]][src_met[j]][k]
-#                    d = create_pathway(namemap, G, r)
-#                    with open('/home/himanshu/Desktop/%s_%s to %s_just one pathway.json'%("_".join(modids), src_met[j], tar_met[i]), 'w') as fp:
-#                        json.dump(d, fp, separators=(',',':'))
-#                    mod = cobra.io.json.load_json_model('/home/himanshu/Desktop/%s_%s to %s_just one pathway.json'%("_".join(modids), src_met[j], tar_met[i]))
-#                    page = flux_map(mod, display_name_format=lambda x: str(x.id), figsize=(1024,500),flux_dict={rxn.id: None for rxn in mod.reactions})
-#                    pathsHTML.append(page)
-#                    label  = "%s %s to %s just one pathway"%("_".join(modids), src_met[j], tar_met[i])
-#                    labels.append(label)
-#                    pathsUnion.append(page)
-#                    labelsUnion.append(label)
-
-#                    pred = G.predecessors
-#                    succ = G.successors
-#                    all_pathways_count = []
-#                    path_count = []
-#                    cyclic_pathway_count = []
-#                    all_reactions_involved = []
-#                    len_scope = len(scope)
-#                    len_scopes.append(len_scope)
-#                    cut_branched_paths = len(all_tar2src[tar_met[i]][src_met[j]])
-#                    cut_branched_pathss.append(cut_branched_paths)
-#                    for plen in pathways[tar_met[i]]:
-#                        all_pathways_count.append(len(pathways[tar_met[i]][plen]))
-#                        if plen <= int(cut_len):
-#                            path_count.append(len(pathways[tar_met[i]][plen]))
-#                        for p in pathways[tar_met[i]][plen]:
-#                            for reactions in p:
-#                                all_reactions_involved.append(reactions)
-#                    if tar_met[i] in cyclic:
-#                        for plen in cyclic[tar_met[i]]:
-#                            if plen <= int(cut_len):
-#                                cyclic_pathway_count.append(len(cyclic[tar_met[i]][plen]))
-#                    all_cnt = sum(all_pathways_count)
-#                    all_cnts.append(all_cnt)
-#                    cut_cnt = sum(path_count)
-#                    cut_cnts.append(cut_cnt)
-#                    cut_cyclic = sum(cyclic_pathway_count)
-#                    cut_cyclics.append(cut_cyclic)
-#                    minsteps = min(pathways[tar_met[i]])
-#                    minstepss.append(minsteps)
-#            else:
-#                wt = {}
-#                for a in range(len(all_tar2src[tar_met[i]][src_met[j]])):
-#                    for b in range(len(all_tar2src[tar_met[i]][src_met[j]][a])):
-#                        wt[all_tar2src[tar_met[i]][src_met[j]][a][b].replace(" ","")] = 0
-#                for a in range(len(all_tar2src[tar_met[i]][src_met[j]])):
-#                    for b in range(len(all_tar2src[tar_met[i]][src_met[j]][a])):
-#                        wt[all_tar2src[tar_met[i]][src_met[j]][a][b].replace(" ","")] += 1
-#                rUnion = set()
-#                for a in range(len(all_tar2src[tar_met[i]][src_met[j]])):
-#                    rUnion.update(all_tar2src[tar_met[i]][src_met[j]][a])
-#                rUnion = list(rUnion)
-#                dUnion = create_pathway(namemap, G, rUnion)
-#                with open('/home/himanshu/Desktop/%s_%s to %s_UNION.json'%("_".join(modids), src_met[j], tar_met[i]), 'w') as fp:
-#                        json.dump(dUnion, fp, separators=(',',':'))
-#                modUnion = cobra.io.json.load_json_model('/home/himanshu/Desktop/%s_%s to %s_UNION.json'%("_".join(modids), src_met[j], tar_met[i]))
-#                solution = modUnion.optimize()
-#                a = 0
-#                for reacts in modUnion.reactions:
-#                    rs = str(reacts.id)
-#                    solution.fluxes[a] = wt[rs]
-#                    a = a+1
-#                pageUnion = flux_map(modUnion, display_name_format=lambda x: str(x.id), figsize=(1024,500),flux_dict= solution)
-#                pathsUnion.append(pageUnion)
-#                labelUnion  = "%s %s to %s UNION"%("_".join(modids), src_met[j], tar_met[i])
-#                labelsUnion.append(labelUnion)
-
-#                pred = G.predecessors
-#                succ = G.successors
-#                all_pathways_count = []
-#                path_count = []
-#                cyclic_pathway_count = []
-#                all_reactions_involved = []
-#                len_scope = len(scope)
-#                len_scopes.append(len_scope)
-#                cut_branched_paths = len(all_tar2src[tar_met[i]][src_met[j]])
-#                cut_branched_pathss.append(cut_branched_paths)
-#                for plen in pathways[tar_met[i]]:
-#                    all_pathways_count.append(len(pathways[tar_met[i]][plen]))
-#                    if plen <= int(cut_len):
-#                        path_count.append(len(pathways[tar_met[i]][plen]))
-#                    for p in pathways[tar_met[i]][plen]:
-#                        for reactions in p:
-#                            all_reactions_involved.append(reactions)
-#                if tar_met[i] in cyclic:
-#                    for plen in cyclic[tar_met[i]]:
-#                        if plen <= int(cut_len):
-#                            cyclic_pathway_count.append(len(cyclic[tar_met[i]][plen]))
-#                all_cnt = sum(all_pathways_count)
-#                all_cnts.append(all_cnt)
-#                cut_cnt = sum(path_count)
-#                cut_cnts.append(cut_cnt)
-#                cut_cyclic = sum(cyclic_pathway_count)
-#                cut_cyclics.append(cut_cyclic)
-#                minsteps = min(pathways[tar_met[i]])
-#                minstepss.append(minsteps)
-
-
-#                for k in range(2):
-#                    print('Most different pathway combination found for %s to %s'%(src_met[j],tar_met[i]))
-#                    r = diff_tar2src[tar_met[i]][src_met[j]][k]
-#                    d = create_pathway(namemap, G, r)
-#                    with open('/home/himanshu/Desktop/%s_%s to %s_%i.json'%("_".join(modids), src_met[j], tar_met[i], k), 'w') as fp:
-#                        json.dump(d, fp, separators=(',',':'))
-#                    mod = cobra.io.json.load_json_model('/home/himanshu/Desktop/%s_%s to %s_%i.json'%("_".join(modids), src_met[j], tar_met[i], k))
-#                    page = flux_map(mod, display_name_format=lambda x: str(x.id), figsize=(1024,500),flux_dict={rxn.id: None for rxn in mod.reactions})
-#                    pathsHTML.append(page)
-#                    label  = "%s %s to %s %i"%("_".join(modids), src_met[j], tar_met[i], k)
-#                    labels.append(label)
-#    return render_template('combi.html', pathsHTML = pathsHTML, labels = labels, pathsUnion = pathsUnion, labelsUnion = labelsUnion, labelsSummary = labelsSummary, len_scopes = len_scopes, cut_branched_pathss = cut_branched_pathss, all_cnts = all_cnts, cut_cnts = cut_cnts, cut_cyclics = cut_cyclics, minstepss = minstepss, cut_len = cut_len, tar_met = tar_met)
-
-##    return render_template('summary.html',len_scope = len_scope, cut_branched_paths = cut_branched_paths, all_cnt = all_cnt, cut_cnt = cut_cnt, cut_cyclic = cut_cyclic, minsteps = minsteps, pathway1 = pathway1, pathway2 = pathway2, src_met = src_met, tar_met = tar_met, cut_len = cut_len )
-
-
-
 @app.route("/test", methods=['GET', 'POST'])
 def test():
     knockForm = Knock(request.form)
     global modids
     if request.method == 'POST':
+    #if request.form.post['action'] == 'make_paths':
        if knockForm.validate():
           global modids
           if len(modids) != 0 :
@@ -369,7 +136,7 @@ def test():
              for i in tar_met:
                  if i == "":
                     tar_met.remove(i)
-             G, namemap = mq.create_graph("/home/himanshu/Desktop/uploads",len(modids))
+             G, namemap = mq.create_graph(os.path.join(app.config['UPLOAD_PATH']),len(modids))
              print('Graph made')
              pathways, cyclic, scope = mq.find_pathways(G,seed_met,cut_len)
              print('pathways found')
@@ -378,6 +145,8 @@ def test():
              check = 0
              for i in range(len(tar_met)):
                  all_reactions[tar_met[i]] = []
+                 pred = G.predecessors
+                 succ = G.successors
                  if tar_met[i] in pathways:
                     all_reactions_involved = []
                     for plen in pathways[tar_met[i]]:
@@ -408,7 +177,8 @@ def test():
                             modids = []
                             modids.append(Jid)
                 fold_name = "_".join(modids)
-                os.makedirs(os.path.join(app.config['UPLOAD_PATH'], fold_name))
+                if not os.path.exists(os.path.join(app.config['UPLOAD_PATH'], fold_name)):
+                    os.makedirs(os.path.join(app.config['UPLOAD_PATH'], fold_name))
                 for i in cobra_mods:
                     cobra.io.write_sbml_model(i, os.path.join(app.config['UPLOAD_PATH'], fold_name + "/" + i.id + ".xml"))
                 G_k, namemap_k = mq.create_graph(os.path.join(app.config['UPLOAD_PATH'], fold_name), len(modids))
@@ -466,4 +236,6 @@ def test():
           return render_template('knockUpload.html', knockForm = knockForm)
 
 if __name__ == "__main__":
-    app.run(use_reloader=True, debug=True, port = 5002)
+    url = 'http://127.0.0.1:5002'
+    webbrowser.open_new(url)
+    app.run()
